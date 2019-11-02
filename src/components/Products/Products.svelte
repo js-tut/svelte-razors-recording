b4806c0381376acc5e8d69ce0e976520905fe46f
john-smilga <johnsmilga@gmail.com>
2019-11-02 15:34:50 -0700

    13-unsubscribe-shorthand

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,20 +1,9 @@
 <script>
   import { onMount, onDestroy } from "svelte";
   import products from "../../stores/defaultProducts";
-  let localProducts = [];
-  let unsubscribe;
-  onMount(() => {
-    unsubscribe = products.subscribe(value => {
-      console.log("subscribed");
-      localProducts = value;
-    });
-  });
-  onDestroy(() => {
-    unsubscribe();
-  });
 </script>
 
 <h1>hello from products component</h1>
-{#each localProducts as product}
+{#each $products as product}
   <h2>{product.title}</h2>
 {/each}
