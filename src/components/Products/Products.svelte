f14272ef6c5d96392eba6c17d49d22db8ebfa34f
john-smilga <johnsmilga@gmail.com>
2019-11-02 16:27:35 -0700

    16-loading-component

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -3,13 +3,17 @@
   import { onMount, onDestroy } from "svelte";
   import products from "../../stores/defaultProducts";
   import Product from "./Product.svelte";
+  import Loading from "../Loading.svelte";
 </script>
+
 
 <section class="section">
   <h2 class="section-title">{title}</h2>
   <div class="products-center">
     {#each $products as product (product.id)}
       <Product {product} />
+    {:else}
+      <Loading />
     {/each}
   </div>
 
