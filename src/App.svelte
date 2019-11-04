3e7b10b40fff04a6441ec0fc6da70e78f4a6f128
john-smilga <johnsmilga@gmail.com>
2019-11-04 08:53:06 -0800

    27-global-store-basics

--- src/App.svelte
+++ src/App.svelte
@@ -11,11 +11,15 @@
   // components
   import Navbar from "./components/Navbar/Navbar.svelte";
   import Sidebar from "./components/Navbar/Sidebar.svelte";
+  // global store
+  import globalStore from "./stores/globalStore";
 </script>
 
 <Router>
   <Navbar />
-  <Sidebar />
+  {#if $globalStore.sidebar}
+    <Sidebar />
+  {/if}
   <Route path="/" component={Home} />
   <Route path="/about" component={About} />
   <Route path="/login" component={Login} />
