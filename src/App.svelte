dced62ce007624439dd3bb05a57d45783abe8bf7
john-smilga <johnsmilga@gmail.com>
2019-11-04 06:39:15 -0800

    21-small-navbar

--- src/App.svelte
+++ src/App.svelte
@@ -8,14 +8,12 @@
   import About from "./pages/About.svelte";
   // router
   import { Router, Route, Link } from "svelte-routing";
+  // components
+  import Navbar from "./components/Navbar/Navbar.svelte";
 </script>
 
 <Router>
-  <nav class="navbar">
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/products">Products</Link>
-  </nav>
+  <Navbar />
   <Route path="/" component={Home} />
   <Route path="/about" component={About} />
   <Route path="/login" component={Login} />
