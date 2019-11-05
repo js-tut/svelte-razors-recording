bdddaee67efb2b3fcf86f6fba625223143b18273
john-smilga <johnsmilga@gmail.com>
2019-11-05 08:57:35 -0800

    37-cart-total

--- src/components/Cart/ItemsList.svelte
+++ src/components/Cart/ItemsList.svelte
@@ -1,6 +1,6 @@
 <script>
   import Item from "./Item.svelte";
-  import cart from "../../stores/cart";
+  import cart, { cartTotal } from "../../stores/cart";
   import { fly } from "svelte/transition";
   import { flip } from "svelte/animate";
 </script>
@@ -18,4 +18,5 @@
       <h2 class="empty-cart">is currently empty...</h2>
     {/each}
   </article>
+  <h3 class="cart-total">total : ${$cartTotal}</h3>
 </section>
