4983a1cd400b8c71ed823abf010e08ec21e6f1f3
john-smilga <johnsmilga@gmail.com>
2019-11-05 08:35:06 -0800

    36-cart-list-transitions

--- src/components/Cart/ItemsList.svelte
+++ src/components/Cart/ItemsList.svelte
@@ -8,7 +8,12 @@
 <section class="cart-items">
   <article>
     {#each $cart as cartItem, index (cartItem.id)}
-      <Item {...cartItem} />
+      <div
+        in:fly={{ delay: (index + 1) * 500, x: 100 }}
+        out:fly={{ x: -100 }}
+        animate:flip>
+        <Item {...cartItem} />
+      </div>
     {:else}
       <h2 class="empty-cart">is currently empty...</h2>
     {/each}
