73f65c28830b65f132c41a577532368b243c1f8d
john-smilga <johnsmilga@gmail.com>
2019-11-06 08:46:30 -0800

    44-login-logout-link

--- src/components/Navbar/Sidebar.svelte
+++ src/components/Navbar/Sidebar.svelte
@@ -3,6 +3,7 @@
   import { link } from "svelte-routing";
   import globalStore from "../../stores/globalStore";
   import { fly, fade } from "svelte/transition";
+  import LoginLink from "../LoginLink.svelte";
 </script>
 
 <div class="sidebar-container" transition:fly={{ x: -1000 }}>
@@ -36,6 +37,9 @@
           </a>
         </li>
       {/each}
+      <li>
+        <LoginLink />
+      </li>
     </ul>
   </div>
 </div>
